b44496e4ecef92c618c9c1d9e3fbac148eb877ed
aliefe04 <aliefe.cakici@std.yeditepe.edu.tr>
2023-01-02 17:41:32 +0300

    olucak

--- fetch.js
+++ fetch.js
@@ -1,6 +1,12 @@
-fetch("https://query1.finance.yahoo.com/v8/finance/chart/ODAS.IS?lang=tr-TR&includePrePost=false&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance",
-    {
-        mode: 'no-cors'
-    }).then(response => {
-        console.log(response);
-    })
+axios.get("https://query1.finance.yahoo.com/v8/finance/chart/ODAS.IS?lang=tr-TR&includePrePost=false&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance", {
+
+    headers:{
+        "Host": "query1.finance.yahoo.com",
+        "Access-Control-Allow-Origin": "*",
+    }
+
+}
+)
+      .then(response => {
+        console.log(response.data.chart.result[0].indicators.quote[0].open[0]);
+})
